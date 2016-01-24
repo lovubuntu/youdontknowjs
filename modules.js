0f8938a4f14d9628ae1fd03c9bc59dfd5921f348
Prabu K <prabuk@thoughtworks.com>
2016-01-24 10:05:36 +0530

    singleton module

--- modules.js
+++ modules.js
@@ -1,4 +1,4 @@
-function nothingUsefulFunction(){
+function notSoUsefulFunction(){
 	var someArray = [1,2,3,4,5];
 	var something = "something something";
 
@@ -10,23 +10,38 @@ function nothingUsefulFunction(){
 		console.log(someArray);
 	}
 }
-x = nothingUsefulFunction();
+x = notSoUsefulFunction();
 
 function coolModule(){
-	var someArray = [1,3,4,5,6];
 	var something = 'something something';
+	var counter = 0;
 	function doSomething(){
 		console.log(something);
 	}
-	function doSomethingElse(){
-		console.log(someArray);
+	function incrementCounter(){
+		counter++;
+		console.log(counter);
 	}
 	return {
 		doSomething: doSomething,
-		doSomethingElse: doSomethingElse
+		incrementCounter: incrementCounter
 	};
 }
 
 var more = coolModule();
 more.doSomething();
-more.doSomethingElse();
+more.incrementCounter();
+var notModifiedByPreviousFunction = coolModule();
+notModifiedByPreviousFunction.incrementCounter();
+
+var singletonCounter = (function singleton(){
+	var counter=0;
+	function incrementCounter(){
+		counter++;
+		console.log(counter);
+	}
+	return {
+		incrementCounter: incrementCounter
+	}
+})();
+singletonCounter.incrementCounter();
